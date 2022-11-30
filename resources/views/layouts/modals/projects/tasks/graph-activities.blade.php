dbc777c3ed2d51717f413768c572b6cc514fd9ad
EdeesonOpina <edeesonpinav4@gmail.com>
2022-11-30 15:51:41 +0800

    Task management module added

---
+++ resources/views/layouts/modals/projects/tasks/graph-activities.blade.php
@@ -0,0 +1,14 @@
+
+<div class="modal fade" id="graph-activities-{{ $project->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-body">
+        <h5 class="modal-title" id="exampleModalLabel">Activity Graph</h5><br>
+
+        <canvas id="line-chart-30-days"></canvas>
+
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
