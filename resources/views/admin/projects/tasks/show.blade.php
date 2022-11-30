caf69631d8d0fa4f7bad1e7f3dd713c5a9824c5f
EdeesonOpina <edeesonpinav4@gmail.com>
2022-11-30 18:14:23 +0800

    Modified gantt chart

--- resources/views/admin/projects/tasks/show.blade.php
+++ resources/views/admin/projects/tasks/show.blade.php
@@ -71,7 +71,7 @@ function drawChart() {
       @else
             data.addRows([
             ['{{ $project_task->id }}', '{{ $project_task->name }}', '{{ $g_status }}',
-             new Date({{ Carbon::parse($project_task->created_at)->format('Y') }}, {{ Carbon::parse($project_task->created_at)->format('m') }}, {{ Carbon::parse($project_task->created_at)->format('d') }}), new Date({{ Carbon::parse($project_task->created_at)->format('Y') }}, {{ Carbon::parse($project_task->created_at)->format('m') }}, {{ Carbon::parse($project_task->created_at)->format('d') }}), null, {{ $percentage }}, null],
+             new Date({{ Carbon::parse($project_task->created_at)->format('Y') }}, {{ Carbon::parse($project_task->created_at)->format('m') }}, {{ Carbon::parse($project_task->created_at)->format('d') }}), new Date({{ Carbon::parse($project_task->created_at)->format('Y') }}, {{ Carbon::parse($project_task->created_at)->format('m') }}, {{ Carbon::parse($project_task->created_at)->format('d') }}), null, null, null],
             ]);
       @endif
   @endforeach
