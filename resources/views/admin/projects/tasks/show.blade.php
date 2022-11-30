1906408919276b73cb388da0fffa34a39f5194f2
EdeesonOpina <edeesonpinav4@gmail.com>
2022-11-30 18:14:39 +0800

    Modified gantt chart

--- resources/views/admin/projects/tasks/show.blade.php
+++ resources/views/admin/projects/tasks/show.blade.php
@@ -68,11 +68,6 @@ function drawChart() {
             ['{{ $project_task->id }}', '{{ $project_task->name }}', '{{ $g_status }}',
              new Date({{ Carbon::parse($project_task->created_at)->format('Y') }}, {{ Carbon::parse($project_task->created_at)->format('m') }}, {{ Carbon::parse($project_task->created_at)->format('d') }}), new Date({{ Carbon::parse($project_task->deadline_date)->format('Y') }}, {{ Carbon::parse($project_task->deadline_date)->format('m') }}, {{ Carbon::parse($project_task->deadline_date)->format('d') }}), null, {{ $percentage }}, null],
             ]);
-      @else
-            data.addRows([
-            ['{{ $project_task->id }}', '{{ $project_task->name }}', '{{ $g_status }}',
-             new Date({{ Carbon::parse($project_task->created_at)->format('Y') }}, {{ Carbon::parse($project_task->created_at)->format('m') }}, {{ Carbon::parse($project_task->created_at)->format('d') }}), new Date({{ Carbon::parse($project_task->created_at)->format('Y') }}, {{ Carbon::parse($project_task->created_at)->format('m') }}, {{ Carbon::parse($project_task->created_at)->format('d') }}), null, null, null],
-            ]);
       @endif
   @endforeach
 
