e6cda9beb1b0dfe60db099be3401dada81fcc0c6
EdeesonOpina <edeesonpinav4@gmail.com>
2022-12-06 16:09:43 +0800

    Modified projects

--- app/Http/Controllers/Admin/BRFController.php
+++ app/Http/Controllers/Admin/BRFController.php
@@ -182,7 +182,7 @@ class BRFController extends Controller
             return back();
         }
 
-        return BudgetRequestForm::latest()->first()->reference_number;
+        return str_replace('BRF-', '', BudgetRequestForm::latest()->first()->reference_number ?? 0) + 1;
 
         $brf_count = str_replace('BRF-', '', BudgetRequestForm::latest()->first()->reference_number ?? 0) + 1; // get the latest brf sequence then add 1
 
